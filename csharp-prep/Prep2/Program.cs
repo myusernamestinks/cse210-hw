20a3090cac73e3d93e4820d356c400a8928497fe
myusernamestinks <104661176+myusernamestinks@users.noreply.github.com>
2024-01-09 21:48:53 -0600

    letter grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}.");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Hurray, You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("There is always next time!");
+        }
+
     }
 }
