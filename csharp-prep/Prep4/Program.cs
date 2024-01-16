c6856d2731100cce74d4286a214799323cf6a441
myusernamestinks <104661176+myusernamestinks@users.noreply.github.com>
2024-01-15 21:27:35 -0600

    created number list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Please enter a series of numbers, type 0 when done.");
+
+        int number;
+        do
+        {
+            Console.Write("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                numbers.Add(number);
+            }
+
+
+        } while (number != 0);
+
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The average is: {numbers.Average()}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
+
+        List<int> positiveNumbers = numbers.Where(n => n > 0).ToList();
+        Console.WriteLine($"The Smallest postive number is: {positiveNumbers.Min()}");
+
+        List<int> sortedList = numbers.OrderBy(n => n).ToList();
+        Console.WriteLine("The sorted list is: ");
+        foreach (int sortedNumber in sortedList)
+        {
+            Console.WriteLine(sortedNumber);
+        }
+
     }
 }
