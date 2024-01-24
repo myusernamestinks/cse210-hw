dae17a710620209725d39349b5e0a36ea94a3b4a
myusernamestinks <104661176+myusernamestinks@users.noreply.github.com>
2024-01-24 12:48:22 -0600

    jobs and resume added.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Paint inspector";
+        job1._company = "Coating Systems and Supply";
+        job1._startYear = 2018;
+        job1._endYear = 2019;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Lead Install Technician";
+        job2._company = "Vivint";
+        job2._startYear = 2019;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jay Brown";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
